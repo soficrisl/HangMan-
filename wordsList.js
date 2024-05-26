a974ec872029bd49b390d2bb42ff71c832d34cf8
soficrisl <sleon@correo.unimet.edu.ve>
2024-05-26 01:21:15 -0400

    CSS MODIFICADO

--- wordsList.js
+++ wordsList.js
@@ -1,15 +1,1 @@
-const wordList = [
-    {
-        word: "casa"
-    },
-    {
-        word: "caoba"
-    },
-    {
-        word: "cuaderno"
-    },
-    {
-        word: "albaricoque"
-    }
-
-];
+const wordList =['Nature', 'Tree', 'River', 'Mountain', 'Forest', 'Ocean', 'Desert', 'Flower', 'Grass', 'Rain', 'Snow', 'Wind', 'Sun', 'Moon', 'Star', 'Earth', 'Sky', 'Cloud', 'Lake', 'Beach', 'Valley', 'Cliff', 'Waterfall', 'Glacier', 'Meadow', 'Hill', 'Canyon', 'Island', 'Volcano', 'Jungle', 'Coral', 'Leaf', 'Sand', 'Storm', 'Thunder', 'Lightning', 'Fog', 'Breeze', 'Tide', 'Pebble', 'Cave', 'Technology', 'Computer', 'Smartphone', 'Internet', 'Software', 'Hardware', 'Network', 'Database', 'Server', 'Cloud', 'Email', 'Robot', 'Artificial Intelligence', 'Virtual Reality', 'Augmented Reality', 'Blockchain', 'Cryptocurrency', 'Drone', 'Gadget', 'Laptop', 'Tablet', 'Screen', 'Keyboard', 'Mouse', 'Printer', 'Scanner', 'WiFi', 'Bluetooth', 'USB', 'Code', 'Algorithm', 'Data', 'Encryption', 'Firewall', 'Malware', 'Software', 'Hardware', 'Operating System', 'Application', 'Program', 'Processor', 'Emotions', 'Happiness', 'Sadness', 'Anger', 'Fear', 'Love', 'Joy', 'Surprise', 'Disgust', 'Trust', 'Anticipation', 'Excitement', 'Anxiety', 'Calm', 'Contentment', 'Frustration', 'Grief', 'Guilt', 'Hope', 'Envy', 'Jealousy', 'Pride', 'Shame', 'Sympathy', 'Compassion', 'Boredom', 'Loneliness', 'Nostalgia', 'Relief', 'Gratitude', 'Admiration', 'Awe', 'Embarrassment', 'Contempt', 'Disappointment', 'Euphoria', 'Serenity', 'Amusement', 'Courage', 'Despair', 'Empathy', 'Food', 'Apple', 'Banana', 'Orange', 'Strawberry', 'Grape', 'Watermelon', 'Pineapple', 'Mango', 'Blueberry', 'Peach', 'Tomato', 'Carrot', 'Broccoli', 'Potato', 'Lettuce', 'Spinach', 'Onion', 'Garlic', 'Pepper', 'Cucumber', 'Bread', 'Rice', 'Pasta', 'Chicken', 'Beef', 'Pork', 'Fish', 'Shrimp', 'Crab', 'Lobster', 'Cheese', 'Milk', 'Yogurt', 'Butter', 'Egg', 'Salt', 'Pepper', 'Sugar', 'Honey', 'Chocolate', 'Sports', 'Soccer', 'Basketball', 'Baseball', 'Tennis', 'Golf', 'Swimming', 'Running', 'Cycling', 'Boxing', 'Wrestling', 'Football', 'Hockey', 'Volleyball', 'Gymnastics', 'Skiing', 'Snowboarding', 'Surfing', 'Skateboarding', 'Archery', 'Fencing', 'Rowing', 'Kayaking', 'Canoeing', 'Sailing', 'Rugby', 'Cricket', 'Badminton', 'Table Tennis', 'Handball', 'Water Polo', 'Lacrosse', 'Squash', 'Equestrian', 'Diving', 'Judo', 'Karate', 'Taekwondo', 'Climbing', 'Triathlon', 'Marathon']; 
